bcfec4bcdb23e10461aa3e456e47c75aab784165
FrancaTM <francatm@outlook.com>
2020-01-10 20:03:53 -0300

    navigate to checkout page

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -99,34 +99,32 @@ class BurgerBuilder extends Component {
 
   purchaseContinueHandler = () => {
     // alert("You continue!");
-
-    this.setState({ loading: true });
-
-    const order = {
-      ingredients: this.state.ingredients,
-      price: this.state.totalPrice,
-      customer: {
-        name: "Tulio",
-        address: {
-          street: "Temp Street",
-          zipCode: "12345",
-          country: "Brazil"
-        },
-        email: "test@test.com.br"
-      },
-      deliveryMethod: "fastest"
-    };
-
-    axios
-      .post("/orders.json", order)
-      .then(response => {
-        // console.log(response);
-        this.setState({ loading: false, purchasing: false });
-      })
-      .catch(error => {
-        // console.log(error);
-        this.setState({ loading: false, purchasing: false });
-      });
+    // this.setState({ loading: true });
+    // const order = {
+    //   ingredients: this.state.ingredients,
+    //   price: this.state.totalPrice,
+    //   customer: {
+    //     name: "Tulio",
+    //     address: {
+    //       street: "Temp Street",
+    //       zipCode: "12345",
+    //       country: "Brazil"
+    //     },
+    //     email: "test@test.com.br"
+    //   },
+    //   deliveryMethod: "fastest"
+    // };
+    // axios
+    //   .post("/orders.json", order)
+    //   .then(response => {
+    //     // console.log(response);
+    //     this.setState({ loading: false, purchasing: false });
+    //   })
+    //   .catch(error => {
+    //     // console.log(error);
+    //     this.setState({ loading: false, purchasing: false });
+    //   });
+    this.props.history.push("/checkout");
   };
 
   componentDidMount() {
